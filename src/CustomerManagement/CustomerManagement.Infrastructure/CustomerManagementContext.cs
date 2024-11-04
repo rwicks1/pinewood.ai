f4cd567ca9f4fa433861bce5b316cf4091d20f83
russell.wicks <russell.wicks@defra.onmicrosoft.com>
2024-11-04 23:36:10 +0000

    Added infrastructure files

---
+++ src/CustomerManagement/CustomerManagement.Infrastructure/CustomerManagementContext.cs
@@ -0,0 +1,19 @@
+﻿using CustomerManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerManagement.Infrastructure;
+
+public class CustomerManagementContext(DbContextOptions<CustomerManagementContext> options) : DbContext(options)
+{
+    public DbSet<Customer> Customers { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Seed initial data
+        modelBuilder.Entity<Customer>().HasData(
+            new Customer{ Id = 1, Name = "Leanne Fairclough", Address = "6 Station Road", PhoneNumber = "01743 845569"}
+        );
+    }
+
+}
---
+++ src/CustomerManagement/CustomerManagement.Infrastructure/Migrations/20241104232001_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Id", "Address", "Name", "PhoneNumber" },
+                values: new object[] { 1, "6 Station Road", "Leanne Fairclough", "01743 845569" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
